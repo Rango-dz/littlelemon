678187c35a705b4f13d003d1728c9caba2449ba6
moroix <freehear@gmail.com>
2024-04-10 09:26:12 +0100

    add meta tags

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>Little Lemmon</h1>
-      </header>
+
     </div>
   );
 }
